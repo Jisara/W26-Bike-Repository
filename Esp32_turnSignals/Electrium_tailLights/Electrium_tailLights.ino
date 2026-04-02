c213e5d715309d51546e7dc15624dff6c3f9f5a8
SChang214 <sophiesee20@gmail.com>
2026-04-01 21:47:14 -0400

    Added circuit tester and updated taillight code
    Tail lights code should be final

--- Esp32_turnSignals/Electrium_tailLights/Electrium_tailLights.ino
+++ Esp32_turnSignals/Electrium_tailLights/Electrium_tailLights.ino
@@ -8,7 +8,13 @@ unsigned long previousMillisRight = 0;
 long intervalLeft = 1000; // blink rate for led1
 long intervalRight = 1000; // blink rate for led2
 
-//unsigned long lastPressedLeft = 0; // tracks when the button was last pressed
+unsigned long lastPressedLeft = 0; // tracks when the button was last pressed
+unsigned long lastPressedRight = 0; // tracks when the button was last pressed
+
+long debounceWait = 10; // amount of time to debounce
+bool debouncedLeft = true; //true == not debounced yet
+bool debouncedRight = true; //true == not debounced yet
+
 
 int leftOUTState = LOW; // state of output
 int rightOUTState = LOW; // state of output
@@ -16,52 +22,67 @@ int rightOUTState = LOW; // state of output
 int leftINState = LOW; // state of the LED
 int rightINState = LOW; // state of the LED
 
+int leftPreviousINState = HIGH; // last recorded state of input
+int rightPreviousINState = HIGH; // last recorded state of input
+
+
 bool leftOn = false; //should the LED be on?
 bool rightOn = false; // should the LED be on?
 
 
 void setup() {
- // put your setup code here, to run once:
   pinMode(turnLeftIN, INPUT_PULLUP);
   pinMode(turnRightIN, INPUT_PULLUP);
   pinMode(turnLeftOUT, OUTPUT);
   pinMode(turnRightOUT, OUTPUT);
 
   Serial.begin(115200);
-  Serial.println("READY!!!");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   unsigned long currentMillis = millis();
+  
+  //reads new button state
   leftINState = digitalRead(turnLeftIN);
   rightINState = digitalRead(turnRightIN);
 
-//2 things -- make buffer before button change registors
-//-- make sure held presses do not constantly flip the state
+//triggers when button is pressed (input state changes)
+  if(leftPreviousINState != leftINState){
+    lastPressedLeft = currentMillis;
+    debouncedLeft = false;
+    leftPreviousINState = leftINState;
+  }
 
+  if(rightPreviousINState != rightINState){
+    lastPressedRight = currentMillis;
+    debouncedRight = false;
+    rightPreviousINState = rightINState;
+  }
 
-  if(leftINState == LOW){
-    delay(500);
-    leftOn = !leftOn; // change state of leftOne
+  //changes boolean that tells the system that the light should be blinking
+  if(leftINState == LOW && !debouncedLeft && currentMillis - lastPressedLeft >= debounceWait){
+    leftOn = !leftOn; 
+    debouncedLeft = true;
 
-    if(!leftOn){
+    //turn light off if button is pressed when light is on
+    if(!leftOn && leftOUTState == HIGH){
       digitalWrite(turnLeftOUT, LOW);
       leftOUTState = LOW;
     }    
   }
 
-  if(rightINState == LOW){
-    delay(500);
-    rightOn = !rightOn; // change state of leftOne
+  if(rightINState == LOW && !debouncedRight && currentMillis - lastPressedRight >= debounceWait){
+    rightOn = !rightOn; 
+    debouncedRight = true;
 
     if(!rightOn){
       digitalWrite(turnRightOUT, LOW);
       rightOUTState = LOW;
+
     }    
   }
 
-
+  //blinks light at specified interval
   if (leftOn == true && currentMillis - previousMillisLeft >= intervalLeft) { //has the interval passed since the last blink
     previousMillisLeft = currentMillis; // update previousMillis (so that it doesn't repeat forever)
     leftOUTState = (leftOUTState == LOW) ? HIGH : LOW; //changes high to low or low to high
@@ -75,7 +96,4 @@ void loop() {
     digitalWrite(turnRightOUT, rightOUTState); //tell the board to send the signal
 
   }
-  
-
 }
-
